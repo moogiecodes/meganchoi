4cf358bf4a4e69b379b6ad4cc02a05bf3fad32f4
moogiecodes <mchoichoi@gmail.com>
2020-05-10 23:41:00 -0700

    Add social icon links, add portfolio pic

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './Styles.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Connect() {
+  const style = {
+    margin: '15% auto',
+    width: '100%',
+    maxHeight: '10em',
+    justifyContent: 'center'
+  }
 
   return (
     <div className="full-page-container section-container">
       <h4>Connect with me!</h4>
-      <p className="mt-5">social links..</p>
+      <span style={style} className="d-inline-flex p-2">
+        <a href="https://github.com/moogiecodes" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon
+            icon={['fab', 'github']}
+            className="d-flex justify-content-center display-4 mx-5" />
+        </a>
+        <a href="https://www.linkedin.com/in/meganchoi/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon
+            icon={['fab', 'linkedin']}
+            className="d-flex justify-content-center display-4 mx-5" />
+        </a>
+        <a href="https://open.spotify.com/user/y52q6hl33nljva7j7bya9h2pe?si=ZvcChfNNRA-fqzsAPfDdEA" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon
+            icon={['fab', 'spotify']}
+            className="d-flex justify-content-center display-4 mx-5" />
+        </a>
+      </span>
     </div>
   );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Styles.css';
 import warblerSS from '../images/warblerSS.png';
 import memgameSS from '../images/memgameSS.png';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import smPomodoro from '../images/smPomodoro.png';
 
 function Portfolio() {
 
@@ -10,7 +10,7 @@ function Portfolio() {
     <div className="section-container">
       <h4 className="mt-5 mb-5">Portfolio</h4>
       <div className="card mb-4">
-        <h3 className="card-header">Warbler</h3>
+        <h3 className="card-header mb-2">Warbler</h3>
         <span className="portfolio-container"><img src={warblerSS} alt="Warbler app homepage screenshot"></img></span>
         <div className="card-body">
           <p className="card-subtitle">A full-stack Twitter-inspired social media application.</p>
@@ -25,7 +25,7 @@ function Portfolio() {
         </div>
       </div>
       <div className="card mb-4">
-        <h3 className="card-header">Memory Card Game</h3>
+        <h3 className="card-header mb-2">Memory Card Game</h3>
         <span className="portfolio-container"><img src={memgameSS} alt="Memory game site screenshot"></img></span>
         <div className="card-body">
           <p className="card-subtitle">A simply designed, single page memory card game. It randomly shuffles cards on restart of the game and tracks cards flipped.</p>
@@ -39,6 +39,23 @@ function Portfolio() {
           </a>
         </div>
       </div>
+      <div className="card mb-4">
+        <h3 className="card-header mb-2">Sailor Moon Pomodoro Timer</h3>
+        <span className="portfolio-container"><img src={smPomodoro} alt="Memory game site screenshot"></img></span>
+        <div className="card-body">
+          <p className="card-subtitle">Sailor Moon-themed pomodoro timer for your lazy Usagi needs!</p>
+          <p className="card-text">Built with: HTML, CSS, Javascript, Jquery</p>
+        </div>
+        <div className="card-body">
+          <a href="https://moogiecodes.github.io/SailorMoonPomodoroTimer/" target="_blank" rel="noopener noreferrer">
+            <button type="button" className="btn btn-outline-info mr-1">Demo</button>
+          </a>
+          <a href="https://github.com/moogiecodes/SailorMoonPomodoroTimer" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-outline-info">Github Repo</button>
+          </a>
+        </div>
+      </div>
+      <p> <span role="img" aria-label="laptop">💻</span> Much more in progress <span role="img" aria-label="exclamation">❗️</span>
+      </p>
     </div>
   );
 }
